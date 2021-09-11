cfea8df61c83a6d408e7dd3d889c980d801ed49e
Shubham <shubhamss102@gmail.com>
2021-09-11 18:07:36 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 /*
 
-	submitted by:
-				Shubham 	204101054
-				Rahul Ballani   204101017
-
+	This assignment is to design a simple File System which makes the following assumptions:
+	 The File system resides on a disk that is 128 KB in size. 
+	 There is only one root directory. No subdirectories are allowed. 
+	 The File system supports at most 16 files. 
+	 The maximum size of a file is 8 blocks where each block is 1KB in size. 
+	 Each File has a unique name
 */
 
 #include<iostream>
